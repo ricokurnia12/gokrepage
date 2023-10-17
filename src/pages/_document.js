b8da6b51853625fa896d664c11ed20183326c56f
ricokurnia12 <ricokurnia19@gmail.com>
2023-10-17 14:11:50 +0700

    fixing GA

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,15 +6,14 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <title>Ganesha Operation | Go Kreasi</title>
-        <meta
-          name="description"
-          content="Aplikasi GO Kreasi memudahkan kamu untuk menerapkan formula 3B: Belajar, Berlatih, dan Bertanding di mana saja dan kapan saja hanya dalam satu genggaman. Membuatmu siap untuk capai target akademikmu."
-        />
+
         <Script
+          strategy="lazyOnload"
           id="gtm-script"
           src="https://www.googletagmanager.com/gtag/js?id=G-GFBCTCXQD2"
         />
-        <script
+        <Script
+          strategy="lazyOnload"
           id="gtm-config"
           dangerouslySetInnerHTML={{
             __html: `
@@ -27,6 +26,10 @@ export default function Document() {
                 gtag("config", "G-GFBCTCXQD2" ,{page_path: window.location.pathname,});
               `,
           }}
+        ></Script>
+        <meta
+          name="description"
+          content="Aplikasi GO Kreasi memudahkan kamu untuk menerapkan formula 3B: Belajar, Berlatih, dan Bertanding di mana saja dan kapan saja hanya dalam satu genggaman. Membuatmu siap untuk capai target akademikmu."
         />
       </Head>
       <body>
